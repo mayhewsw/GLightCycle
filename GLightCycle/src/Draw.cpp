82beb6703602f27996ef05968f4bd4880e0ba017
Sam Kim <kimsj@rose-hulman.edu>
2011-10-30 20:17:40 -0400

    Added Draw class

---
+++ GLightCycle/src/Draw.cpp
@@ -0,0 +1,17 @@
+/*
+ * Draw.cpp
+ *
+ *  Created on: Oct 30, 2011
+ *      Author: kimsj
+ */
+
+#include "Draw.h"
+
+Draw::Draw() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Draw::~Draw() {
+	// TODO Auto-generated destructor stub
+}
---
+++ GLightCycle/src/Draw.h
@@ -0,0 +1,19 @@
+/*
+ * Draw.h
+ *
+ *  Created on: Oct 30, 2011
+ *      Author: kimsj
+ */
+
+#ifndef DRAW_H_
+#define DRAW_H_
+
+#include "World.h"
+
+class Draw {
+public:
+	Draw();
+	virtual ~Draw();
+};
+
+#endif /* DRAW_H_ */
